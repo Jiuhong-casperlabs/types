454199ccd37d4a1297f2d625cc2074d5648d0d9f
jiuhong <jiuhong@casperlabs.io>
2021-09-07 16:39:59 +0800

    modify storemap

--- contract/src/storemap.rs
+++ contract/src/storemap.rs
@@ -15,15 +15,13 @@ pub extern "C" fn call() {
     let value1 = (123, true, "hello");
     let value2 = (999, false, "world");
     let mut map = BTreeMap::new();
-    let mut map2 = BTreeMap::new();
 
     map.insert(key1.clone(), value1.clone());
     map.insert(key2.clone(), value2.clone());
-    map2.insert("aaa", &map);
 
     let uref = storage::new_uref(map);
-    let uref2 = storage::new_uref(map2);
-    runtime::put_key("testmap", uref.into());
+
+    runtime::put_key("testmap1", uref.into());
 }
 
 //reference https://github.com/casper-network/casper-node/blob/e2027dbe979ebf91f10ba8a90ffba1fddbd9fb09/types/src/cl_value/jsonrepr.rs
